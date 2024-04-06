48fe149d759f66c97b6d085a7e800d694a1ba3a8
Davide Visco <davidevisco@email.com>
2024-04-06 19:29:04 +0200

    refactor: bookview page and related components

---
+++ src/app/components/BookView/BookView.tsx
@@ -0,0 +1,36 @@
+import { db } from '@/common/db'
+import { State } from '@/lib/store'
+import { useSelector } from 'react-redux'
+import { CardViewToggler } from '../CardViewToggler/CardViewToggler'
+import { Card } from '../Card/Card'
+import { BookMedia } from '@/common/types'
+
+import bookCover from '@/images/k_-_nel_mare_del_tempo.jpg'
+import { classNames } from '@/common/classNames'
+
+export const BookView = () => {
+  const book: BookMedia = db.books[0]
+  const bookView = useSelector((state: State) => state.bookView.value)
+
+  return (
+    <section>
+      <CardViewToggler />
+
+      <div
+        className={[
+          'mt-4 grid gap-8',
+          bookView === 'list' ? 'mx-auto grid-cols-1' : 'grid-cols-7',
+        ]}
+      >
+        <Card
+          mediaURI={bookCover.src}
+          title={book.title}
+          description={book.description}
+          actions={book.genres}
+          status={book.status.name}
+        />
+        <Card title={book.title} />
+      </div>
+    </section>
+  )
+}
---
+++ src/app/components/Card/Card.tsx
@@ -0,0 +1,81 @@
+import Image from 'next/image'
+import '@/common/string.extensions'
+import variables from '@/app/variables.module.scss'
+import { useSelector } from 'react-redux'
+import { State } from '../../../lib/store'
+
+import noImage from '@/images/no-image.png'
+
+export const Card = ({
+  title,
+  mediaURI = '',
+  description = '',
+  actions = [],
+  status = '',
+}: {
+  title: string
+  mediaURI?: string
+  description?: string
+  actions?: string[]
+  status?: string
+}) => {
+  const bookView = useSelector((state: State) => state.bookView.value)
+
+  return (
+    <div className="indicator w-auto">
+      {status && (
+        <span
+          className="indicator-item badge"
+          style={{ backgroundColor: variables[status.toCSSClass()] }}
+        ></span>
+      )}
+
+      <div
+        className={[
+          'card w-full bg-neutral shadow-xl',
+          bookView === 'list' && 'card-side',
+        ]}
+      >
+        <figure>
+          <Image
+            className={['object-contain', bookView === 'list' && 'h-52 w-auto']}
+            src={mediaURI ? mediaURI : noImage.src}
+            width={128}
+            height={182}
+            alt={title}
+          />
+        </figure>
+
+        <div
+          className={[
+            'card-body text-pretty ',
+            bookView === 'list' && 'grow shrink basis-full',
+          ]}
+        >
+          <h2 className="card-title">{title.toCapitalize()}</h2>
+          <p
+            className={[
+              'pt-1 text-neutral-content line-clamp-3 ',
+              bookView === 'grid' && 'hidden',
+            ]}
+          >
+            {description}
+          </p>
+
+          <div className="card-actions justify-end">
+            {bookView === 'list' &&
+              actions.map((action) => (
+                <button
+                  key={action}
+                  className="badge badge-outline"
+                  style={{ borderColor: variables[action.toCSSClass()] }}
+                >
+                  {action.toCapitalize()}
+                </button>
+              ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
