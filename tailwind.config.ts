132cfed32eb675508f3dc5d737fd95b2a3069922
Davide Visco <davidevisco@email.com>
2024-04-06 18:21:11 +0200

    chore: add tailwind and daisyui theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,37 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [require("daisyui")],
-};
-export default config;
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#b21e35',
+          secondary: '#2c6e49',
+          accent: '#216869',
+          neutral: '#2D3430',
+          'base-100': '#2C2523',
+          info: '#9ba9ff',
+          success: '#95d387',
+          warning: '#f9b16e',
+          error: '#fd7272',
+        },
+      },
+    ],
+  },
+  plugins: [require('daisyui')],
+}
+export default config
